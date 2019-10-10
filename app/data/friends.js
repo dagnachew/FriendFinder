fe589fc4edcf47e1346d66346715aa3eeaaacce0
Seifu <dagnachew@gmail.com>
2019-10-10 07:24:35 -0500

    check if it is working

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,24 +4,70 @@
 // Initially we just set it equal to a "dummy" friened.
 // ===============================================================================
 
-var friendArray = [
+const friends = [
   {
     "name":"Ahmed",
     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
+    "scores":[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  }, 
+
+  {
+    "name":"Jane",
+    "photo":"https://images.unsplash.com/photo-1464863979621-258859e62245?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    "scores":[3, 4, 5, 2, 1, 1, 5, 3, 4, 5]
+  },
+
+  {
+    "name":"Nik",
+    "photo":"https://images.unsplash.com/photo-1476814607969-0dfe98b9be5c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    "scores":[2, 1, 1, 4, 4, 5, 2, 3, 1, 3]
+  },
+
+  {
+    "name":"Brooke",
+    "photo":"https://images.unsplash.com/photo-1492447216082-4726bf04d1d1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    "scores":[5, 5, 5, 4, 4, 5, 2, 2, 4, 2]
+  },
+
+  {
+    "name":"Paola",
+    "photo":"https://images.unsplash.com/photo-1500832333538-837287aad2b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    "scores":[5, 2, 2, 4, 4, 4, 5, 5, 1, 5]
+  },
+
+  {
+    "name":"Sally",
+    "photo":"https://images.unsplash.com/photo-1524511858955-5d3bf3afd42e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    "scores":[5, 1, 1, 4, 1, 1, 2, 4, 1, 3]
+  },
+
+  {
+    "name":"Dane",
+    "photo":"https://images.unsplash.com/photo-1545912453-3d32e20f72bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    "scores":[3, 3, 3, 4, 4, 5, 5, 4, 1, 5]
+  },
+
+  {
+    "name":"Warren",
+    "photo":"https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    "scores":[5, 2, 5, 4, 4, 5, 4, 3, 5, 3]
+  },
+
+  {
+    "name":"Ayo",
+    "photo":"https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    "scores":[5, 1, 4, 4, 4, 5, 1, 3, 1, 5]
+  },
+
+  {
+    "name":"Chris",
+    "photo":"https://images.unsplash.com/photo-1441622915984-05d13dfb3d8c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    "scores":[4, 3, 1, 4, 4, 5, 5, 3, 2, 3]
   }
+
   ];
+
+
   
   // Note how we export the array. This makes it accessible to other files using require.
-  module.exports = friendArray;
+  module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,7 +3,8 @@
 // We are linking our routes to a series of "data" sources.
 // ===============================================================================
 
-const friendsData = require("../data/friends");
+const friends = require("../data/friends");
+const path = require('path');
 
 
 // ===============================================================================
@@ -17,22 +18,83 @@ module.exports = function(app) {
     // ---------------------------------------------------------------------------
   
 
-app.get("/api/friends", (req, res) => {
-    res.json(friendsData);
-});
+    app.get("/api/friends", (req, res) => {
+        res.json(friends);
+    });
 
-app.post("/api/friends", function(req, res) {
-    // Note the code here. Our "server" will respond to requests.
-    // It will do this by sending out the value "true" have a table.
-    // req.body is available since we're using the body parsing middleware.
-    if (friendsData.length < 5) {
-      friendsData.push(req.body);
-      res.json(true);
+    app.post("/api/friends", function(req, res) {
+        // Note the code here. Our "server" will respond to requests.
+        // It will do this by sending out the value "true" have a table.
+        // req.body is available since we're using the body parsing middleware.
+        let Score = 25;
+        let bff;
+
+        //loop through friends array  
+        for(let i = 0; i < friends.length; i++){
+
+          //Array for matches
+          let newArr = [];
+        
+          for(let j = 0; j < friends[i].scores.length; j++){
+
+          //calculate difference
+          newArr.push(Math.abs(friends[i].scores[i] - req.body.scores[j]));
+          } 
+
+          let match = newArr.reduce((a, b) => a + b, 0);
+
+            if (match < Score) {
+              Score = match;
+              bff = friends[i];
+            }
+        }
+
+        // to user
+        res.json(bff);
+
+        // to array
+        friends.push(req.body);
+          
+    });
+
+  }
+
+
+  app.post("/api/friends", function(req, res) {
+
+    // User details
+    let user = req.body;
+
+    // convert scores
+    for(let i = 0; i < user.scores.length; i++) {
+      user.scores[i] = parseInt(user.scores[i]);
     }
-    else {
-      waitListData.push(req.body);
-      res.json(false);
+
+    let bbf = 0;
+    let minimumDifference = 40;
+
+    // loop
+        for(let i = 0; i < friends.length; i++) {
+    
+      let totalDifference = 0;
+
+    //   compare the user and the each friend scores
+      for(let j = 0; j < friends[i].scores.length; j++) {
+        var difference = Math.abs(user.scores[j] - friends[i].scores[j]);
+        totalDifference += difference;
+      }
+
+      if(totalDifference < minimumDifference) {
+        bbf = i;
+        minimumDifference = totalDifference;
+      }
     }
+
+    // push user to friend array
+    friends.push(user);
+
+    // display the best friend match
+
+    res.json(friends[bbf]);
+
   });
-
-};
